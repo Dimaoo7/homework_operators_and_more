0bc6a7aba0ff57958aebcc92c724eb6001d58aed
Dimaoo7 <dimakrjukov@gmail.com>
2023-01-30 20:39:36 +0100

    первая ветка

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Привет");
 
+        //Первое задание
 
-    }
+       int age = 13;
+
+        if (age >= 18 ) {
+            System.out.println("Тебе уже больше 18 лет");
+        }
+        if (age <= 17) {
+            System.out.println("Тебе еще нет 18 лет, подожди " + ( 18 - age )+ " лет / года");
+        }
+
+        }
+
 }
