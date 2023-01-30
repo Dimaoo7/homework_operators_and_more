35a311c41e872018d8949f5fecdef6b35e17fbd4
Dimaoo7 <dimakrjukov@gmail.com>
2023-01-30 20:41:03 +0100

    З задания сделано

--- src/Main.java
+++ src/Main.java
@@ -12,6 +12,30 @@ public class Main {
             System.out.println("Тебе еще нет 18 лет, подожди " + ( 18 - age )+ " лет / года");
         }
 
+        //Второе задание
+        int temperetura = -2;
+
+        if (temperetura >= 5) {
+
+            System.out.println("Сегодня тепло " + temperetura + " Градуса");
         }
+        if (temperetura <= 5) {
+            System.out.println("Апчи, а сегодня холодно " + temperetura + " Градуса");
+        }
+
+        //Третье задание
+        int speed = 120;
+        if (speed >= 60) {
+            System.out.println("Ты привысил скорость, твоя скорость сейчас " +speed+ " км в час");
+        }
+        if (speed <= 60) {
+            System.out.println("Ты не привысил скорость, твоя скорость сейчас " +speed+ " км в час");
+        }
+
+        // Четвертая задача
+
+
+
+    }
 
 }
